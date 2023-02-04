98e60ccfc8adfa4b14581c0f24bbe4f7db4b0071
HongYen <yenvt9a1@gmail.com>
2023-02-04 09:28:17 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -14,5 +15,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+// handler 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Serve Error",
+    });
+});
 
 module.exports = app;
